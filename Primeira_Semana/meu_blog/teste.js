9f3525ecff1aeb8226eb38dd770dc201406ebc9d
PatriciaClares <dev.patricia.c@gmail.com>
2020-04-20 18:58:44 -0300

    primeiro commit

---
+++ Primeira_Semana/meu_blog/teste.js
@@ -0,0 +1,48 @@
+let a;
+let b;
+let c; 
+
+a = 3; 
+b = 5; 
+c = a * b + 5; 
+a = a + 8 / 2; 
+b = 5 * b * 5; 
+console.log(a,b,c);
+
+let salario = 1150; 
+let imp; 
+
+if (salario < 1000){
+    imp = 0; 
+} else{
+imp = (salario - 150) * 15/100; 
+}
+console.log(imp); 
+
+let a1 = 5;
+let b2;
+let c3; 
+
+b2 = 0 
+c3= 1 
+while(a1 != 0){
+b2 = b2 + 1 
+c3= c3 * 2 
+a1 = a1 - 1 
+}
+
+console.log(a1,b2,c3)
+
+let NUM = 9; let P; let I;
+P = 0 
+I = 0 
+while(NUM > 0){ 
+if (NUM % 2 == 0){ 
+P = P + 1 
+} else { 
+I = I + 1 
+}
+NUM = NUM - 1 
+}
+console.log(P,I)
+
